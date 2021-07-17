58604e2b25f36c00eef534c3ed8c67456f095fb4
ginty1 <chiombagintykondwani@gmail.com>
2021-07-17 20:57:13 +0200

    app js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
+import { login, logout, selectUser } from "./features/appSlice";
+import Priview from "./Priview";
+import WebcamCapture from "./WebcamCapture";
+import Login from "./Login.js";
+import { useEffect } from "react";
+import { auth } from "./db/firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app_body">
+            <Switch>
+              <Route path="/priview">
+                <Priview />
+              </Route>
+              <Route path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route path="/chats">
+                <Chats />
+              </Route>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
+      </Router>
     </div>
   );
 }
-
 export default App;
